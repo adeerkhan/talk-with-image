c21e9facc01ae161ea022396b3123b4c013b3d79
Adeer Khan <adeerkhan@hotmail.com>
2023-08-16 19:16:14 +0500

    fixed errors and made changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from tempfile import NamedTemporaryFile
+
 import streamlit as st
 from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
 from tools import ImageCaptionTool, ObjectDetectionTool
 
 
@@ -10,48 +12,50 @@ from tools import ImageCaptionTool, ObjectDetectionTool
 tools = [ImageCaptionTool(), ObjectDetectionTool()]
 
 conversational_memory = ConversationBufferWindowMemory(
-    memory_key= 'chat_history',
+    memory_key='chat_history',
     k=5,
     return_messages=True
 )
 
 llm = ChatOpenAI(
-    openai_api_key= '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',
+    openai_api_key='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',
     temperature=0,
-    model_name = "gpt-3.5-turbo"
+    model_name="gpt-3.5-turbo"
 )
 
 agent = initialize_agent(
-    agent = "chat-conversational-react-description",
-    tools = tools,
-    llm = llm,
-    max_iterations = 5,
-    verbose = True,
-    memory = conversational_memory,
-    early_stoppy_method = 'generate'
+    agent="chat-conversational-react-description",
+    tools=tools,
+    llm=llm,
+    max_iterations=5,
+    verbose=True,
+    memory=conversational_memory,
+    early_stopping_method='generate'
 )
 
-# setting up the title
-st.title('Ask something from the image')
+# set title
+st.title('Ask a question to an image')
 
-# setting up the header
+# set header
 st.header("Please upload an image")
 
-# upload the file
-file = st.file_uploader(" ", type=["jpeg", "jpg", "png"])
+# upload file
+file = st.file_uploader("", type=["jpeg", "jpg", "png"])
 
 if file:
-    # displaying the image
+    # display image
     st.image(file, use_column_width=True)
 
-    # text input for the question
-    user_question = st.text_input('Ask a question from your image')
+    # text input
+    user_question = st.text_input('Ask a question about your image:')
+
+    # compute agent response
     with NamedTemporaryFile(dir='.') as f:
         f.write(file.getbuffer())
         image_path = f.name
 
         # write agent response
         if user_question and user_question != "":
-            with st.spinner(text="In Progress..."):
+            with st.spinner(text="In progress..."):
                 response = agent.run('{}, this is the image path: {}'.format(user_question, image_path))
                 st.write(response)
